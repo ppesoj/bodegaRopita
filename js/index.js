d114c63aedd308fa7c7a6cec1dd8e6f8cd9a4767
chava <azulyjose@hotmail.com>
2023-03-11 23:42:03 -0600

    si

--- js/index.js
+++ js/index.js
@@ -47,27 +47,3 @@ function mostrarRopa () {
         }
     });
 }
-
-$(buscar_datos());
-function buscar_datos(consulta){
-    // $.ajax({
-    //     url: 'controlador/buscar.php',
-    //     type: 'POST',
-    //     dataType: 'html',
-    //     data: {consulta: consulta},
-    // })
-    // .done(function(respuesta){
-    //     $("#datos").html(respuesta);
-    // })
-    // .fail(function(){
-    //     console.log("error");
-    // })
-}
-$(document).on('keyup','#caja_busqueda', function(){
-    var valor = $(this).val();
-    if(valor != ""){
-        buscar_datos(valor);
-    } else{
-        buscar_datos();
-    }
-});
