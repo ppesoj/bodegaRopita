1460714283fea674855d7e596361b04ced9737cc
chava <azulyjose@hotmail.com>
2023-03-11 23:26:01 -0600

    ya encuentra la ropa por qr

--- controlador/catalogo.php
+++ controlador/catalogo.php
@@ -7,23 +7,50 @@
     ini_set('display_startup_errors', '1');
     error_reporting(E_ALL);
 
-    $recoger = "SELECT foto, modelo, codigo, cantidad, precio FROM productos;";
-    $nvConexion = nuevaConexion();
-    $query_obtener = mysqli_query($nvConexion, $recoger);
-    if (!$query_obtener) {
-        $resultados["fallo"] = mysqli_error($nvConexion);
-        $resultados["status"] = false;
+    $accionCatalogo = $_POST["func"];
+
+
+    if($accionCatalogo == "mostrarRopa") {
+        $recoger = "SELECT foto, modelo, codigo, cantidad, precio FROM productos;";
+        $nvConexion = nuevaConexion();
+        $query_obtener = mysqli_query($nvConexion, $recoger);
+        if (!$query_obtener) {
+            $resultados["fallo"] = mysqli_error($nvConexion);
+            $resultados["status"] = false;
+            return print (json_encode($resultados));
+        }
+        if ( isset($query_obtener) ) {
+            while ($fila = $query_obtener->fetch_assoc()) {
+                $resultados["data"][]=$fila;
+            }
+            $resultados["status"] = true;
+        } else {
+            $resultados["status"] = false;
+        }
+        $nvConexion->close();
         return print (json_encode($resultados));
     }
-    if ( isset($query_obtener) ) {
-        while ($fila = $query_obtener->fetch_assoc()) {
-            $resultados["data"][]=$fila;
+
+    if($accionCatalogo == "buscarPrenda") {
+        $codigo = $_POST["codigo"];
+        $recoger = "SELECT foto, modelo, codigo, cantidad, precio FROM productos WHERE modelo = '".$codigo."';";
+        $nvConexion = nuevaConexion();
+        $query_obtener = mysqli_query($nvConexion, $recoger);
+        if (!$query_obtener) {
+            $resultados["fallo"] = mysqli_error($nvConexion);
+            $resultados["status"] = false;
+            return print (json_encode($resultados));
         }
-        $resultados["status"] = true;
-    } else {
-        $resultados["status"] = false;
+        if ( isset($query_obtener) ) {
+            while ($fila = $query_obtener->fetch_assoc()) {
+                $resultados["data"][]=$fila;
+            }
+            $resultados["status"] = true;
+        } else {
+            $resultados["status"] = false;
+        }
+        $nvConexion->close();
+        return print (json_encode($resultados));
     }
-    $nvConexion->close();
-    return print (json_encode($resultados));
 
 ?>
