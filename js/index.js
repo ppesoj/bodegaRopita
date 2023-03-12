81eea764d7cf7bafd35fbb1c5772ecdc2b764128
yicsbs <52188097+yicsbs@users.noreply.github.com>
2023-03-11 20:18:39 -0600

    boton de busqueda

--- js/index.js
+++ js/index.js
@@ -18,19 +18,20 @@ function mostrarRopa () {
             if(respuesta["status"]) {
                 console.log(respuesta)
 
-                //insertar la ropita a la tabla
+                //insertar la ropita a la tabla               
                     var objNoticias = respuesta;
                     Object.keys(objNoticias.data).forEach((key, index) => {
                         //console.log(objNoticias.data[index]);
                         var miniatura = `
                             <tr>
                                 <td>
-                                    <img src="controlador/docs/ropa/${objNoticias.data[index].foto}" style="    width: 8rem;" >
+                                    <img src="controlador/docs/ropa/${objNoticias.data[index].foto}" style="width: 8rem;" >
                                 </td>
                                 <td>${objNoticias.data[index].modelo}</td>
                                 <td>${objNoticias.data[index].codigo}</td>
                                 <td>${objNoticias.data[index].descripcion}</td>
                                 <td>${objNoticias.data[index].precio}</td>
+                                <td>${objNoticias.data[index].precioOferta}</td>
                             </tr>
                         `;
                         $("#cuerpoCatalogo").append(miniatura);
@@ -45,5 +46,28 @@ function mostrarRopa () {
             console.log("ERROR : ", e);
         }
     });
-
 }
+
+$(buscar_datos());
+function buscar_datos(consulta){
+    $.ajax({
+        url: 'controlador/buscar.php',
+        type: 'POST',
+        dataType: 'html',
+        data: {consulta: consulta},
+    })
+    .done(function(respuesta){
+        $("#datos").html(respuesta);
+    })
+    .fail(function(){
+        console.log("error");
+    })
+}
+$(document).on('keyup','#caja_busqueda', function(){
+    var valor = $(this).val();
+    if(valor != ""){
+        buscar_datos(valor);
+    } else{
+        buscar_datos();
+    }
+});
