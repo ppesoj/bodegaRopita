76c3c79700e98c2e3e144d94a42cd698b819f102
chava <azulyjose@hotmail.com>
2023-03-18 15:37:08 -0600

    ya inserta registros, falta subir imagen y el qr

--- controlador/dashboard.php
+++ controlador/dashboard.php
@@ -16,38 +16,33 @@
         }
         
         //recibimos los datos y no son vacios
-        if (empty($_POST["titulo"]) || empty($_POST["noticia"]) || empty($_POST["func"]) || ($_POST["portada"]) == "undefined") {
-            $output = json_encode(array('type' => 'errorIncom', 'text' => 'Datos incompletos pequeño fetito :,v'));
-            die($output);
-        }
-
-        //validacion de token
-        $miToken = $_POST["token"];
-        $resulToken = JWT::decode($_POST['token'], 'secret_server_key');
-        if( isset($resulToken->error) ) {
-            $output = ['type' => 'errorfatal', 'text' => 'Que andas haciendo? >:V'];
-            return print (json_encode($output));
-            exit;
-        }
-
-        //llamada a la funcion
-        $func = $_POST["func"];
-        if($func == "addNoticia") {
-            $titulo = $_POST["titulo"]; $noticia = $_POST["noticia"]; $portada = $_POST["portada"]; $txtMiniatura = $_POST["txtMiniatura"];
-            $id = $resulToken->id;
-            InsertarNoticia($conexion, $titulo, $noticia, $portada, $txtMiniatura,$id);
-        } else {
-            $output = json_encode(array('type' => 'error', 'text' => 'Ocurrio un problema con tus acciones >:v'));
-            die($output);
-        }
+        if (
+                empty($_POST["func"]) ||
+                empty($_POST["inputModelo"]) || 
+                empty($_POST["inputCodigo"]) || 
+                empty($_POST["inputCantidad"]) || 
+                empty($_POST["inputPrecioConvert"]) || 
+                empty($_POST["inputPrecio-ofertaConvert"])
+                // ($_POST["portada"]) == "undefined"
+            ) {
+                $output = json_encode(array('type' => 'errorIncom', 'text' => 'Datos incompletos pequeño fetito :,v'));
+                die($output);
+            }
     }
 
-    function InsertarNoticia ($conexion,$titulo, $noticia, $portada, $txtMiniatura,$id) {
+    if($_POST["func"] == "agregarProducto"){
         $resultados = ["status" => false];
-        $idNoticia = 0;
+        $idRegreso = 0;
+
+        $inputModelo = $_POST["inputModelo"];
+        $inputCodigo = $_POST["inputCodigo"];
+        $inputCantidad = $_POST["inputCantidad"];
+        $inputPrecio = $_POST["inputPrecioConvert"];
+        $inputPrecioOferta = $_POST["inputPrecio-ofertaConvert"];
+        $inputDescripcion = $_POST["inputDescripcion"];
 
         //consulta insertar la noticia
-        $query = $conexion->query("CALL InsertarNoticia('$titulo','$noticia','$txtMiniatura','$id')");
+        $query = $conexion->query('INSERT INTO productos (id, modelo, codigo, descripcion, cantidad, foto, precio, precioOferta, piezasPaquete, created_at, updated_at, dataQR) VALUES (NULL, "'.$inputModelo.'", "'.$inputCodigo.'", "'.$inputDescripcion.'", '.$inputCantidad.', NULL, '.$inputPrecio.', '.$inputPrecioOferta.', NULL, NULL, NULL, NULL);');
         if (!$query) {
             printf("Error: %s\n", mysqli_error($conexion));
             echo "la db ha fallado :c";
@@ -55,118 +50,17 @@
         }
         if ( isset($query) ) {
 
-            while ($fila = $query->fetch_assoc()) {
-                $idNoticia = $fila["id"];
-            }
+            var_dump($conexion);
+            $idRegreso = $conexion->insert_id;
+            $resultados["idRegreso"] = $idRegreso;
             
             $resultados["status"] = true;
         } else {
             $resultados["status"] = false;
         }
         $conexion->close();
-
-            $ext_formatos = array("png","jpg","jpeg","webp");
-
-            //guardado de  portada
-            $imgPorta = json_decode($_POST["portada"]);
-            $extensionPorta = explode('/', mime_content_type($imgPorta->result))[1];
-            if(!in_array(strtolower($extensionPorta),$ext_formatos)) {
-                $resultados["status"] = false;
-                $resultados["images"] = "El tipo de imagen de portada no es correcto";
-                return print (json_encode($resultados));
-            }else {
-                $imgPorta = str_replace('data:image/'.$extensionPorta.';base64,', '', $imgPorta->result);
-
-                $imgPorta = str_replace(' ', '+', $imgPorta);
-                //descodificar de base 64
-                $dataPorta = base64_decode($imgPorta);
-                $fileNamePorta = $idNoticia . '-portada.' . $extensionPorta;
-                $dir_to_save = "docs/noticias/";
-                $addPortada = $dir_to_save.$fileNamePorta;
-                file_put_contents($addPortada,$dataPorta);
-
-                if(!file_exists($addPortada)) {
-                    $resultados["images"]["portada"]["file"] = "No";
-                }else {// si se agrego con exito la guardamos en la base de datos con su noticia asociada
-                    $resultados["images"]["portada"]["file"] = "Si";
-    
-                    $insertarPorta = "UPDATE noticias SET nombrePortada = '$fileNamePorta' WHERE id = $idNoticia;";
-    
-                    $nvConexionPort = nuevaConexion();
-                    $queryNoticiaPort = mysqli_query($nvConexionPort, $insertarPorta);
-                    if (!$queryNoticiaPort) {
-                        $resultados["images"]["portada"]["fallo"] = mysqli_error($nvConexionPort);
-                    }
-                    if ( isset($queryNoticiaPort) ) {
-                        $resultados["images"]["portada"]["status"] = true;
-                    } else {
-                        $resultados["images"]["portada"]["status"] = false;
-                    }
-
-                    $nvConexionPort->close();
-                }
-
-            }
-
-            
-            //guardado de imagenes del carrucel
-            $arrayImgs = json_decode($_POST['imgs']);
-            foreach ($arrayImgs as $clave => $valor) {
-                $img = $valor->result;
-                $extension = explode('/', mime_content_type($img))[1];
-                if(!in_array(strtolower($extension),$ext_formatos)) {
-                    // print_r("$extension no paso");
-                    $resultados["status"] = false;
-                    $resultados["images"] = "Los tipos de imagenes no son correctos";
-                    return print (json_encode($resultados));
-                }
-            }
-
-            foreach ($arrayImgs as $clave => $valor) {
-                $img = $valor->result;
-                $extension = explode('/', mime_content_type($img))[1];
-                $img = str_replace('data:image/'.$extension.';base64,', '', $img);
-                $img = str_replace(' ', '+', $img);
-                //descodificar de base 64
-                $data = base64_decode($img);
-                //nombre de la imagen
-                //$fileName = uniqid() . '.png';
-                $fileName = $idNoticia."-".uniqid() . '.' . $extension;
-
-                //direccion de guardado
-                $dir_to_save = "docs/noticias/";
-
-                if(!file_exists($dir_to_save)) {
-                    mkdir($dir_to_save,755,true);
-                }
-                
-                $add = $dir_to_save.$fileName;
-                file_put_contents($add,$data);
-
-                if(!file_exists($add)) {
-                    $resultados["images"][$fileName]["file"] = "No";
-                }else {// si se agrego con exito la guardamos en la base de datos con su noticia asociada
-                    $resultados["images"][$fileName]["file"] = "Si";
-
-                    $insertar = "INSERT INTO `imagenes`(`nombre`, `idNoticia`) VALUES ('".$fileName."','".$idNoticia."')";
-
-                    $nvConexion = nuevaConexion();
-                    $queryNoticia = mysqli_query($nvConexion, $insertar);
-                    if (!$queryNoticia) {
-                        $resultados["images"][$fileName]["fallo"] = mysqli_error($nvConexion);
-                    }
-                    if ( isset($queryNoticia) ) {
-                        $resultados["images"][$fileName]["status"] = true;
-                    } else {
-                        $resultados["images"][$fileName]["status"] = false;
-                    }
-                    $nvConexion->close();
-
-                }
-
-            }
         
         return print (json_encode($resultados));
     }
-        
+
 ?>
--- vendor/simple-mask-money-master/examples/3.x.x/react/README.md
+++
@@ -1,8 +0,0 @@
-# SimpleMaskMoney Exemple React
-First, install it.
-```shell
-  npm i simple-mask-money --save
-```
-Then, use it as follows:
-#(Not Implemented)
-Help-me to implement
