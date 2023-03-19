a8b3cd89114d638035182039258e3ea5ef98deeb
yicsbs <52188097+yicsbs@users.noreply.github.com>
2023-03-19 00:24:14 -0600

    print JS

---
+++ js/printQR.js
@@ -0,0 +1,14 @@
+function imprimir(data){
+    console.log(data);
+    printJS()
+}
+
+printJS({
+    printable: ['vistas/qr.jpg', 'vistas/qr.jpg', 'vistas/qr.jpg'],
+    type: 'image',
+    imageStyle: 'width:100%;margin-bottom:10px;'
+})
+
+// $("#btnImprimir").on('click', function (e) {
+    
+// });
